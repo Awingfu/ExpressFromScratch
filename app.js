2a0622ae9061d62f7ede1aae32c8b204e86418e6
T <Lui@Ts-MacBook-Air.local>
2018-12-24 14:26:46 -0600

    added view in pug

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require("express")
+const express = require('express');
+const path = require('path');
 
+// Init app
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Hello World");
+// Load view engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug')
+
+// Default route
+app.get('/', (req,res) => {
+    res.render("index")
+    // res.send('Hello World');
 });
 
+// Port to start server on
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+    console.log('Server started on port 3000');
 });
